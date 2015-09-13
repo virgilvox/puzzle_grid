8e4e931000e3a96a8ad94be11f13f3794127a458
virgilvox <moheeb.zara@citrix.com>
2015-09-13 12:48:57 -0700

    added some rest stuff

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 
 angular.module('MyApp')
-.controller('AppCtrl', function($scope) {
+.controller('AppCtrl', function($scope, $http) {
 
   // ALL THE IMPORTANT VARIABLES
 
@@ -13,7 +13,7 @@ angular.module('MyApp')
 
   this.colorTiles = (function() {
     var tiles = [];
-    
+
     for (var i = 0; i < post_count; i++) {
       tiles.push({
         color: pickColor(i),
@@ -24,7 +24,36 @@ angular.module('MyApp')
     return tiles;
   })();
 
+
+
+  function loadPosts () {
+    console.log('this thing');
+  var key = "api_key=HLIFUzqaly1F7T5plo6pv8FnZCINJMIYdGBYjtpZNwpzmfeaGM";
+  var api = "https://api.tumblr.com/v2/blog/ivoirians.tumblr.com/";
+  var retrieve_more = function (offset) {
+    $http.get(api + "posts/text?callback=?&filter=text&limit=20&offset=" + offset + "&" + key).
+      then(function(response) {
+        if (response.posts.length == 20) {
+            retrieve_more(offset + 20);
+        }
+        console.log(response);
+      }, function(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
+
+  };
+
+  retrieve_more(0);
+}
+
+loadPosts();
+
+
   $scope.clicked = function($index){
+
+
+
 
     var win = window.open(puzzles.response.posts[$index].post_url, '_blank');
     win.focus();
