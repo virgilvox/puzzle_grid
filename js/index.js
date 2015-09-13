c49408166d471c39bb1989c3bf66c0a1eb3a79d5
virgilvox <moheeb.zara@citrix.com>
2015-09-13 15:27:40 -0700

    WOW

--- js/index.js
+++ js/index.js
@@ -7,39 +7,70 @@ angular.module('MyApp')
   var popular_color = '#03A9F4';
   var less_popular = '#212121';
   //var post_count = puzzles.response.total_posts;
-  var post_count = 20;
+  var post_count;
   var square_size = 1;
+  //$scope.loadGrid = false;
 
 
-  this.colorTiles = (function() {
-    var tiles = [];
+  loadPosts(function(posts) {
 
-    for (var i = 0; i < post_count; i++) {
-      tiles.push({
-        color: pickColor(i),
-        colspan: square_size,
-        rowspan: square_size
-      });
-    }
-    return tiles;
-  })();
+  $scope.allTiles = posts;
+  console.log(posts);
+
+});
 
 
 
-  function loadPosts () {
-    console.log('this thing');
-  var key = "api_key=HLIFUzqaly1F7T5plo6pv8FnZCINJMIYdGBYjtpZNwpzmfeaGM";
-  var api = "https://api.tumblr.com/v2/blog/ivoirians.tumblr.com/";
-  var retrieve_more = function (offset) {
-    $http.get(api + "posts/text?callback=?&filter=text&limit=20&offset=" + offset + "&" + key).
+  function loadPosts (callback) {
+
+    var posts = [];
+
+    var retrieve_more = function (offset) {
+
+    var key = "api_key=AtAx2nkLkrkUJUE6bLu2upJ1HkJBsQ7sYZEyr9Acc9voec6nQd";
+    var api = "https://api.tumblr.com/v2/blog/ivoirians.tumblr.com/";
+    var url = api + "posts/text?filter=text&limit=20&offset=" + offset + "&" + key;
+
+    var req = {
+   "method": 'GET',
+   "url": url,
+   "headers": {
+     "Content-Type": "application/json"
+   }
+  };
+
+    $http(req).
       then(function(response) {
-        if (response.posts.length == 20) {
+
+        var resp = response.data.response;
+        post_count = resp.total_posts;
+        //console.log(resp);
+        angular.forEach(resp.posts, function(data) {
+          var value = {
+            "color": pickColor(data.note_count),
+            "colspan": square_size,
+            "rowspan": square_size,
+            "title": data.title,
+            "body": data.body,
+            "tags": data.tags,
+            "post_url": data.post_url ,
+            "slug": data.slug,
+            "node_count": data.note_count
+          };
+          this.push(value);
+        }, posts);
+
+        if (resp.posts.length == 20) {
+          if(offset < resp.total_posts){
             retrieve_more(offset + 20);
+          }
+        }else{
+            console.log(posts);
+            $scope.loadGrid = true;
+            callback(posts);
         }
-        console.log(response);
+
       }, function(response) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
       });
 
   };
@@ -47,13 +78,9 @@ angular.module('MyApp')
   retrieve_more(0);
 }
 
-loadPosts();
 
 
   $scope.clicked = function($index){
-
-
-
 
     var win = window.open(puzzles.response.posts[$index].post_url, '_blank');
     win.focus();
@@ -62,7 +89,7 @@ loadPosts();
 
 
   function pickColor(i) {
-    var count = puzzles.response.posts[i].note_count;
+    var count = i;
     var color;
 
     if(count >= 2){
